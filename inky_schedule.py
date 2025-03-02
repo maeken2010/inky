760967e90b9787eab4bb1245a832ba3a87a7622b
maeken2010 <maeken2010@gmail.com>
2025-03-02 22:42:27 +0900

    change schedule

--- inky_schedule.py
+++ inky_schedule.py
@@ -8,10 +8,7 @@ import inky_show
 def job():
     inky_show.show()
 
-#1分毎のjob実行を登録
-schedule.every(1).minutes.do(job)
-
-# schedule.every(3).hours.do(job)
+schedule.every(3).hours.do(job)
 
 while True:
     schedule.run_pending()
